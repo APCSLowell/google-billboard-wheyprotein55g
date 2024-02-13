4da3b7fe32d7b85b1244d728cf7243e34f031f63
wheyprotein55g <143123848+wheyprotein55g@users.noreply.github.com>
2024-02-12 21:06:27 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,34 @@
-
-
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
+  for(int i = 2; i< e.length()-1; i++){
+    double dNum = Double.parseDouble(e.substring(i,i+10));
+      if(isPrime(dNum) == true) {
+        System.out.println(dNum);
+        break;
+      }
+  }
 	}
 	
 
 	//Finish this function
-	public boolean isPrime(double d){
+	public static boolean isPrime(double dNum){
+    if(dNum < 2) return false;
+    for(int i = 2; i <= Math.sqrt(dNum); i++)
+      if(dNum%i ==0) return false;  
+    return true;  
 	}
 
 }
 
+
+
+
+
+
+
+
+
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
